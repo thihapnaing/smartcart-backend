7fc79b3edf87d2833c6d473e437827cae6b84b4d
Sheng Qi <sandyteksq@gmail.com>
2026-08-04 22:02:09 +0800

    Added GET /api/cart, GET /api/orders/{id} endpoints

--- src/main/java/nus/iss/smartcart/backend/repository/PaymentRepository.java
+++ src/main/java/nus/iss/smartcart/backend/repository/PaymentRepository.java
@@ -1,7 +1,11 @@
 package nus.iss.smartcart.backend.repository;
 
 import nus.iss.smartcart.backend.model.Payment;
+import nus.iss.smartcart.backend.model.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(Long orderId);
 }

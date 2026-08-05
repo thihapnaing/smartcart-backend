0b68c398db187a7fbfe22c8e7a3517a18398aa9b
Htet Nandar <htetnandar.282198@gmail.com>
2026-08-05 15:47:29 +0800

    Implement cart retrieval and quantity update functionality

--- src/main/java/nus/iss/smartcart/backend/dto/CartItemDetail.java
+++ src/main/java/nus/iss/smartcart/backend/dto/CartItemDetail.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 public class CartItemDetail {
 
     private Long cartItemId;
+    private Long productVariantId; // Author: Htet Nandar (Grace)
     private String productName;
     private String imageUrl;
     private String size;

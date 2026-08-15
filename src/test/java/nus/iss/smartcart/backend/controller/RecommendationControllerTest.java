3afaf3de781eb3d8a57b3a84fbf36ee9b137cbaf
cecilzhou221 <cecil.chew221@gmail.com>
2026-08-16 02:47:09 +0800

    fix: bypass security filters in recommendation controller test

--- src/test/java/nus/iss/smartcart/backend/controller/RecommendationControllerTest.java
+++ src/test/java/nus/iss/smartcart/backend/controller/RecommendationControllerTest.java
@@ -5,6 +5,7 @@ import nus.iss.smartcart.backend.repository.UserRepository;
 import nus.iss.smartcart.backend.service.RecommendationOrchestratorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RecommendationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class RecommendationControllerTest {
 
     @Autowired
